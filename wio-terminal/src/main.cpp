478116422cea895492d8bb67c85fff785a4b4f71
nowakpawel <kontakt.nowakpawel@gmail.com>
2026-07-29 14:04:23 +0200

    feat: LCD text formatting

--- wio-terminal/src/main.cpp
+++ wio-terminal/src/main.cpp
@@ -3,6 +3,10 @@
 
 TFT_eSPI tft;
 
+void setUpCursor(int i, int fontHeigh) {
+	tft.setCursor(50, i*fontHeigh);
+}
+
 void setup() {
 	tft.begin();
 	tft.setRotation(3);
@@ -10,13 +14,14 @@ void setup() {
 
 	tft.setTextColor(TFT_BLACK);
 	tft.setTextSize(3);
-	tft.setCursor(10,10);
+	// tft.setCursor(10,10);
 
 	Wire.begin();
 	uint8_t addresses[2] = {0x18, 0x19};
 	size_t length = sizeof(addresses) / sizeof(addresses[0]);
 
 	for (int i = 0; i < length; i++) {
+		setUpCursor(i, tft.fontHeight());
 		Wire.beginTransmission(addresses[i]);
 		uint8_t result = Wire.endTransmission();
 
