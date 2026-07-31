776d3ce5e18ad29774597c0434608b044dd079a8
nowakpawel <kontakt.nowakpawel@gmail.com>
2026-07-31 10:50:00 +0200

    feat: read WHO_AM_I from LIS3DH on Wire1 (0x33 confirmed)

--- wio-terminal/src/main.cpp
+++ wio-terminal/src/main.cpp
@@ -2,32 +2,35 @@
 #include <Wire.h>
 
 TFT_eSPI tft;
+const uint8_t LIS3DH_ADDR = 0x18;
+const uint8_t WHOAIM_LIS3DHTR_REGISTER = 0x0F;
 
-void setUpCursor(int i, int fontHeigh) {
-	tft.setCursor(50, i*fontHeigh);
-}
+
+
 
 void setup() {
-	tft.begin();
-	tft.setRotation(3);
-	tft.fillScreen(TFT_BLUE);
+    tft.begin();
+    tft.setRotation(3);
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE);
+    tft.setTextSize(3);
+    // tft.setCursor(50, 0);
 
-	tft.setTextColor(TFT_BLACK);
-	tft.setTextSize(3);
-	// tft.setCursor(10,10);
+    Wire1.begin();
 
-	Wire.begin();
-	uint8_t addresses[2] = {0x18, 0x19};
-	size_t length = sizeof(addresses) / sizeof(addresses[0]);
+    Wire1.beginTransmission(LIS3DH_ADDR);
+    Wire1.write(WHOAIM_LIS3DHTR_REGISTER);
+    uint8_t tx = Wire1.endTransmission(true);
+    uint8_t got = Wire1.requestFrom(LIS3DH_ADDR, 1);
 
-	for (int i = 0; i < length; i++) {
-		setUpCursor(i, tft.fontHeight());
-		Wire.beginTransmission(addresses[i]);
-		uint8_t result = Wire.endTransmission();
+    uint8_t whoami = Wire1.read();
 
-		tft.printf("0x%02X: %d\n", addresses[i], result);
-	}
+    tft.print("tx: "); tft.println(tx);
+    tft.print("got "); tft.println(got);
+    tft.print("0x"); tft.println(whoami, HEX);
+
 }
+
 
 void loop() {
 
